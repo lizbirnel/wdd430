41474bfa100185c6160ddc677baf3bf36fbaf079
lizbirnel <lizbirnel@byui.edu>
2021-09-26 00:27:26 -0600

    wdd 430 repository

---
+++ basics-assignment-2-start/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  username = '';
+ 
+
+  usernameEmpty() {
+    if (this.username==="") {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  onAddUsername() {
+   
+    this.username= "";
+  }
+
+}
+
+
